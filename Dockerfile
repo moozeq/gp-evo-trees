df1c1cc0d36467772f4ab22b4a2642b4980d4e86
Amon <j.niedzw@gmail.com>
2021-01-25 21:09:49 +0100

    [Pipeline] Docker - fix issue with workdir

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM ubuntu:latest
 
 ENV DEBIAN_FRONTEND=noninteractive
 
+WORKDIR /evopipe
+
 COPY requirements.txt requirements.txt
 COPY pipe.py pipe.py
 
 RUN apt-get update && apt-get install -y curl
 RUN bash -c "$(curl -fsSL https://raw.githubusercontent.com/moozeq/GP_EvoTrees/pipeline/setup.sh)"
 
-WORKDIR /evopipe
+ENTRYPOINT ["/miniconda/bin/python3", "pipe.py"]
--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ apt-get update && apt-get upgrade -y && apt-get install -y wget git gcc make tar
 git clone https://github.com/ChrisCreevey/clann.git
 cd clann
 chmod +x install-sh
-sed -i '3236s/.*/                        while(string[i] != \'(\' && string[i] != \')\' && string[i] != \',\' && string[i] != \';\' && string[i] != \':\')/' treecompare2.c
+PATCH_LINE="                        while(string[i] != '(' && string[i] != ')' && string[i] != ',' && string[i] != ';' && string[i] != ':')"
+sed -i "3236s/.*/${$PATCH_LINE}/" treecompare2.c
 ./configure
 #make
 # need to install manually due to some errors
@@ -17,6 +18,8 @@ gcc -g -O2 -Wall -funroll-loops -o clann treecompare2.o -lreadline -lncurses -lm
 mv clann /bin/clann
 cd ..
 
+echo "[+] Installed clann"
+
 # install raxml
 git clone https://github.com/stamatak/standard-RAxML.git
 cd standard-RAxML
@@ -25,11 +28,15 @@ rm *.o
 mv raxmlHPC-PTHREADS-AVX /bin/raxml
 cd ..
 
+echo "[+] Installed RAxML"
+
 # install ninja
 curl http://nimbletwist.com/software/ninja/files/ninja.tgz > ninja.tgz
 tar -xvf ninja.tgz
 mv ninja_*/ninja /bin/ninja
 mv ninja_*/Ninja.jar /bin/Ninja.jar
+
+echo "[+] Installed ninja"
 
 # Get Miniconda and make it the main Python interpreter
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /miniconda.sh
@@ -39,3 +46,7 @@ rm /miniconda.sh
 
 # install packages
 /miniconda/bin/conda install -c bioconda -c conda-forge -c etetoolkit -c anaconda --file requirements.txt
+
+echo "[+] Installed miniconda with packages"
+
+exec $SHELL
